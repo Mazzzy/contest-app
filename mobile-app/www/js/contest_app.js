22706b9f508510d160d18941ad4c4d6163a64c95
Mazahar Shaikh <mazahar.shaikh@wu.com>
2019-01-23 19:07:16 +0530

    Added custom app js and initial map structure

---
+++ mobile-app/www/js/contest_app.js
@@ -0,0 +1,46 @@
+/**
+ * Contest App - Main Behavioral Layer
+ * features - 
+ * show google maps using cordova map plugin
+ * Displays markers based on the co-ordinates
+ * On click of markers displays info - realtime votes count
+ * On click of info displays feature to vote by swiping left /right
+ */
+var testLat = 21.1591857,
+  testLng = 72.7522564;
+
+var contestApp = {
+    data: [],
+    init: function() {
+        // initialise map plugin
+        plugin.google.maps.environment.setEnv({
+            'API_KEY_FOR_BROWSER_RELEASE': '',
+            'API_KEY_FOR_BROWSER_DEBUG': ''
+        });
+
+        // init and generate map
+        this.initGoogleMaps();
+    },
+    initGoogleMaps: function(){
+        var div = document.getElementById("gmap");
+
+        var options = {
+            camera: {
+            target: {lat: testLat, lng: testLng},
+            zoom: 10
+            }
+        };
+
+        // Initialize the map view
+        map = plugin.google.maps.Map.getMap(div, options);
+
+        // Wait until the map is ready status.
+        map.addEventListener(plugin.google.maps.event.MAP_READY, this.onMapReady.bind(this));
+    },
+    onMapReady: function(){
+        map.addMarker({
+            position: {lat: testLat, lng: testLng},
+            title: 'my location'
+        })
+    }
+};
